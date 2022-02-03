a0dc7dfaaf754470698fe4281de17887e2a00008
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-02-03 03:55:26 +0100

    fix can't read property city

--- app.js
+++ app.js
@@ -97,4 +97,7 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Server running  on port ${PORT}`));
 
-
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err);
+});
